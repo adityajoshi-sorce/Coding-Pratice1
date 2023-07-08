0c3f57c6b9d28f9f8618a5b8d856000f9896a551
adityajoshi-sorce <joshiaditya0727@gmail.com>
2023-07-08 14:44:09 +0530

    Safe Codeing

---
+++ script.js
@@ -0,0 +1,11 @@
+const adityaMass = 65;
+const adityMinHeight = 5.9;
+const vijyaMass = 75;
+const vijyaMinHeight = 5.7;
+
+const total = adityaMass/adityMinHeight ** 2;
+console.log(total);
+const total2 = vijyaMass/(vijyaMinHeight * vijyaMinHeight);
+console.log(total2);
+
+console.log(total<total2);
